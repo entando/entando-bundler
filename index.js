6695ae5a1564967e7291505fd102f2c424778326
Kerruba <luca.cherubin@gmail.com>
2020-01-07 18:25:19 +0100

    Updates the lib and add the ability to actually install the custom resource in a specific namespace

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const program = require('commander');
-const registry = require('./modules/registry');
-const {Bundle, Tag, Details} = require('./modules/models');
-const _ = require('lodash');
+const { generateBundle } = require('./lib/actions');
 const version = require('./package.json').version;
-const yaml = require('js-yaml');
 
 function validateOrExit(validation) {
     if (!validation) {
@@ -12,41 +9,7 @@ function validateOrExit(validation) {
     }
 }
 
-function generateModuleName(name, fallbackModule) {
-    if (!name) {
-        name = cleanModuleName(fallbackModule)
-    }
-    return name;
-}
 
-function cleanModuleName(module) {
-    let name = module.name;
-    if (name.startsWith('@')) {
-        let scopeEnd = name.indexOf('/',0);
-        name = name.substr(scopeEnd+1);
-    }
-    return name;
-}
-
-function generate(obj) {
-    console.log(yaml.safeDump(obj));
-}
-
-function buildCustomResources(mods, options) {
-    if (!_.isArray(mods)) {
-        mods = [mods];
-    }
-    const name = generateModuleName(options.bundleName, mods[0]);
-    const metadata = {
-        'name': name,
-        ...(options.bundleNamespace && { 'namespace': options.bundleNamespace })
-    };
-    const details = Details(mods[0]);
-    const tags = mods.map(m => Tag(m))
-        .filter(t => !_.isEmpty(t));
-    generate(Bundle(metadata, details, tags), { 'dryRun': options.dryRun || false });
-    return mods;
-}
 
 program.version(version).name('@entando/de-cli');
 
@@ -56,24 +19,20 @@ program
     .option('--name <bundleName>', 'The name to give to the EntandoDigitalExchangeBundle')
     .option('--namespace <bundleNamespace>', 'The namespace where the EntandoDigitalExchangeBundle will be created')
     .option('--registry <registry>', 'The registry to use for searching the module, by default uses the registry configured in your .npmrc')
-    .action((module, cmdObj) => {
-        registry.getBundleInfo({'name': module, 'registry':cmdObj.registry})
-            .then(mods => {
-                mods = buildCustomResources(mods, cmdObj);
-            })
-            .catch(err => console.error(`An error occurred while retrieving package ${module}`, err));
-    });
+    .option('--dry-run', 'Print the output instead of create the custom resource automatically')
+    .action(generateBundle);
 
-program
-    .command('convert')
-    .description('Convert a JSON string retrieved from an npm view command into EntandoDigitalExchangeBundle custom resource')
-    .option('--json <json>', 'The input is a json string')
-    .option('--file <file>', 'The input is a file')
-    .option('--name <bundleName>', 'The name of the output EntandoDigitalExchangeBundle')
-    .option('--namespace <bundleNamespace>', 'The namespace of the generated EntandoDigitalExchangeBundle')
-    .action((cmdObj) => {
-        buildCustomResources(JSON.parse(cmdObj.json), cmdObj)
-    });
+// Is this actually required?
+// program
+//     .command('convert')
+//     .description('Convert a JSON string retrieved from an npm view command into EntandoDigitalExchangeBundle custom resource')
+//     .option('--json <json>', 'The input is a json string')
+//     .option('--file <file>', 'The input is a file')
+//     .option('--name <bundleName>', 'The name of the output EntandoDigitalExchangeBundle')
+//     .option('--namespace <bundleNamespace>', 'The namespace of the generated EntandoDigitalExchangeBundle')
+//     .action((cmdObj) => {
+//         buildCustomResources(JSON.parse(cmdObj.json), cmdObj)
+//     });
 
 program.parse(process.argv);
 validateOrExit(program.args.length > 0);
